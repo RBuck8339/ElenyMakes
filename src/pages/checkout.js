434bba0c0a81b81f7923cdc912a246a33db5064a
RBuck8339 <101837795+RBuck8339@users.noreply.github.com>
2026-02-09 18:01:44 -0500

    Fixed checkout page Netlify issues

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,15 +1,9 @@
-import purchaseHandler from '../logic/purchaseHandler';
 
 export default function Checkout({items}){
-    // Need successful purchase handling logic
-    let successful_purchase = false;
-    let info = null;  // Tmp
-    // How do i call the function with a param for button
+    
     return (
         <div className="">
-            <input className="" placeholder="Email"/>
-            <input className="" placeholder="Payment stuff"/>
-            <button className="" onClick={purchaseHandler(info)}/>
+            
         </div>
     )
 }

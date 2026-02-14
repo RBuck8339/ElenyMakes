7381c30be5a0d9fec7bf2d5063755700c226c1c5
RBuck8339 <101837795+RBuck8339@users.noreply.github.com>
2026-02-13 20:00:19 -0500

    Removed now unecessary file

--- src/components/cookieStatement.js
+++
@@ -1,9 +0,0 @@
-export default function cookieStatement(){
-    // Just tells users that we will use their cookies in order to store what items are in their cart since we are not making an account mandatory
-    // Fade all other content slightly
-    return (
-        <div className="align-items-center">
-
-        </div>
-    );
-}

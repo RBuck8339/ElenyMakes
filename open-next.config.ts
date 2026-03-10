36ff8d36b905e4839eb6ff38117324616c605dca
RBuck8339 <101837795+RBuck8339@users.noreply.github.com>
2026-03-09 23:03:29 -0400

    Apparently this will build

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
-});
+  // @ts-ignore - This ignores the error in VS Code and the build
+  externalPackages: ["@react-email/render"],
+} as any);
--- src/components/receiptTemplate.js
+++ src/components/receiptTemplate.js
@@ -1,6 +1,14 @@
-import { 
-  Html, Body, Container, Text, Tailwind, Section, Row, Column, Head, Font 
-} from '@react-email/components';
+import { Body } from '@react-email/body';
+import { Container } from '@react-email/container';
+import { Head } from '@react-email/head';
+import { Hr } from '@react-email/hr';
+import { Html } from '@react-email/html';
+import { Img } from '@react-email/img';
+import { Preview } from '@react-email/preview';
+import { Section } from '@react-email/section';
+import { Text } from '@react-email/text';
+// If you use Tailwind:
+import { Tailwind } from '@react-email/tailwind';
 
 export const ReceiptEmailTemplate = ({ items, orderId, num_emails }) => (
   <Html>

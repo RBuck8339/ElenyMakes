2e74f41c3098ed7d7bf4fd2e4be756329c3f6a5d
RBuck8339 <101837795+RBuck8339@users.noreply.github.com>
2026-03-11 22:21:27 -0400

    uninstalled react render

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,5 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({
-  overrides: {
-    wrapper: "cloudflare-node", // Ensures it uses Node-compatible bundling
-    converter: "edge",
-  },
-  // We add it to both places to be safe
-  externalPackages: ["@react-email/render", "@react-email/components"],
-  build: {
-    minify: false, // Disabling minify can sometimes reveal the real path error
-  }
+  externalPackages: ["@react-email/render"], 
 } as any);
